732f6f08d0bd24f1e7d2ef5fd388940406741d9e
Nigel <nindustriestech@gmail.com>
2024-06-28 22:04:00 +0800

    Added login page

---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+import "./page.css"
+
+export default function Home() {
+  return (
+    <section>
+        <div>
+            <div className="greeting-section">
+                <h1>Welcome back</h1>
+                <p>Sign in to your account</p>
+            </div>
+            <div className="credential-field">
+                <div className="input-container">
+                    <label>Username</label>
+                    <input placeholder="Enter username"/>
+                </div>
+                <div className="input-container">
+                    <label>Password</label>
+                    <input type="password" placeholder="•••••••••••••••"/>
+                </div>
+            </div>
+            <button className="primary-button">Sign In</button>
+        </div>
+        
+    </section>   
+  );
+}
